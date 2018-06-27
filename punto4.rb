33ac6a446b14e4c68bb6accec25a69d0ab9bf4fb
DESKTOP-FJUEANT\FMLAACOSTAPARRA <wilson.acosta32@gmail.com>
2018-06-26 22:07:39 -0500

    update

--- punto4.rb
+++ punto4.rb
@@ -5,12 +5,12 @@ class Hash
 end
 
 class Array
-  def get_mode
+  def mode
     (inject(Hash.new(0)) { |h,e| h[e] += 1; h }.ninvert.max ||
 [[]]).last
   end
 end
 
-p [3, 1, 1, 2, 4,2,3,2].get_mode
-p [1,2,3,4].get_mode
+p [3, 1, 1, 2, 4,2,3,2].mode
+p [1,2,3,4].mode
 
